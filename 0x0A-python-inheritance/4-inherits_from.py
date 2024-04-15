564205e8cf64b7d10663342d7cb2cc3056a5caf5
root <root@kirolos>
2024-04-15 19:54:53 +0200

    f

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,10 +8,10 @@ def inherits_from(obj, a_class):
     """from the specified class; otherwise False."""
     cls = type(obj)
 
-    # Base case: Check if the class is exactly the specified class
+    """ Base case: Check if the class is exactly the specified class"""
     if cls is a_class:
         return True
-    # Recursively check parent classes
+    """Recursively check parent classes"""
     for base_class in cls.__bases__:
         if inherits_from(base_class(), a_class):
             return True
