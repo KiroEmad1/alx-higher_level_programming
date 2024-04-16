dd36a374ff30f3d8154332b27450790367e745c2
root <root@kirolos>
2024-04-16 20:08:38 +0200

    f

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/user/bin/python
+"""    function that writes a string to a text file"""
+
+def write_file(filename="", text=""):
+    """""reads a text file(UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
