12cf78152b0d6ea0a27957d836590044cc1cd12b
root <root@kirolos>
2024-04-16 20:13:02 +0200

    f

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,6 @@
 
 
 def write_file(filename="", text=""):
-    """returns the number of chars written to "filename" from "text" """
+"""returns the number of chars written to "filename" from "text" """
     with open(filename, 'w', encoding='utf=8') as f:
         return f.write(text)
