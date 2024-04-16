3c5709a3c428875b991c5487704f52e9ddc736ee
root <root@kirolos>
2024-04-16 20:29:56 +0200

    f

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,7 +5,7 @@ function that appends a string
 
 
 def append_write(filename="", text=""):
-      """eturns the number of characters added:"""
+    """eturns the number of characters added:"""
     with open(filename, 'a', encoding='utf-8') as file:
         file.write(text)
         return len(text)
